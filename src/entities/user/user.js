ef9642e62012ca831feb395216d61ae5a9d207b8
RuslanR <rusjlan28@gmail.com>
2022-07-02 16:55:08 +0300

    git ignor update

--- src/entities/user/user.js
+++ src/entities/user/user.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
-import { StaticQuery, graphql } from "gatsby"
-
+import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
-// import { images, isRetina } from 'entities/cart/mocs/logo-img-db'
 import './user.css'
 
-
-// const [webp] = images.filter(item => item.id === 'webp');
-// const [png] = images.filter(item => item.id === 'png');
-
 export const UserProfile = ( ) => {
-    
+
+  const { contentfulCustomer } = useStaticQuery(graphql`
+  {
+    contentfulCustomer {
+      avatar {
+        gatsbyImageData(placeholder: BLURRED, height: 48, formats: AUTO)
+        file {
+          url
+          fileName
+          contentType
+        }
+      }
+    }
+  }  
+`)
+
+    const image = getImage(contentfulCustomer.avatar)
+
+  console.log('image >>>', image);
 
     const [user] = useState({name: 'Ann', surname: 'Rojkova'})
 
@@ -20,12 +32,7 @@ export const UserProfile = ( ) => {
                 <p className="gretting-name__greetting">Привет</p>
                 <p className="gretting-name__name">{user.name + ' ' + user.surname}</p>
             </div>
-               
-            {/* <picture className='img-wrap'>
-                <source type="image/webp" srcSet={isRetina(webp.webp1x, webp.webp2x)}/>
-                <source type="image/webp" srcSet={isRetina(png.png1x, png.png2x)}/>
-                <img className='img-wrap__img-rounded' src={png.png1x} srcSet={png.png2x} alt="Картинка"/>
-            </picture> */}
+            <GatsbyImage className='img-wrap__img-rounded' image={image} alt={contentfulCustomer.avatar.file.fileName}/>
         </div>
     );
 };
